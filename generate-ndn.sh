79b5cbff50884be628a6417138a74a5247f427f0
Jan Romann <jan.romann@hs-emden-leer.de>
2024-08-20 13:22:47 +0200

    fixup! WIP: Add first version of NDN protocol binding template

--- generate-ndn.sh
+++ generate-ndn.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/bash
+
+# Script that creates JSON Schema, ontology, and JSON-LD context files from the
+# LinkML definition file ndn.yaml.
+#
+# To be able to use this script, you need to set up LinkML on your machine,
+# preferably in a virtual environment, as detailed here in the following
+# tutorial: https://linkml.io/linkml/intro/tutorial01.html
+#
 # TODO: This file will eventually be removed/replaced/moved somewhere else
 
 gen-owl bindings/protocols/ndn/ndn.yaml > bindings/protocols/ndn/ontology.ttl
